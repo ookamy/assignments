417071c03ef0361b68bf5c75aa63ae1518cc1bbf
unknown <sant0125@WJ310-09.mmserver.ottawa.ad.algonquincollege.com>
2012-06-04 12:54:52 -0400

    #1

---
+++ Dino/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>04-06</title>
+	</head>
+
+	<body>
+		<h2>Veloceraptor</h2>
+		<dl>
+			<dt>Loves meat</dt>
+			<dd>1</dd>
+			<dt>In JP</dt>
+			<dd>1</dd>
+		</dl>
+
+	</body>
+</html>
